271a809980ce5211ff4cb7b0f1597ae1a9b9c57d
anouar garbaa <g.anouar@yahoo.com>
2024-10-21 19:29:39 +0100

    add auth slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -24,25 +24,49 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // configureStore() function, like createStore(), creates a store but it makes merging multiple reducers into one reducer easier.
-
-
-
-
-
-
-
-
-
-
 
 //& without reduxjs/toolkit   (we use simply redux)
 // import { createStore } from "redux";
